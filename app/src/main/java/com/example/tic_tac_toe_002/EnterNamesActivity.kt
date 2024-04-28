6a86b6f932b437b6e4165af109b17eead1bfe7e8
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-04-28 20:43:06 +0530

    TT | Final Updates

--- app/src/main/java/com/example/tic_tac_toe_002/EnterNamesActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe_002/EnterNamesActivity.kt
@@ -1,14 +1,25 @@
 package com.example.tic_tac_toe_002
 
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import com.example.tic_tac_toe_002.databinding.ActivityEnterNamesBinding
 
 class EnterNamesActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEnterNamesBinding
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var btnDefaultMusic: MediaPlayer
+    private var isMuted: Boolean = false
+    private var isSoundOff : Boolean = false
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +27,32 @@ class EnterNamesActivity : AppCompatActivity() {
         binding = ActivityEnterNamesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sharedPreferences = getSharedPreferences("HangmanPrefs", Context.MODE_PRIVATE)
+
+        isMuted = sharedPreferences.getBoolean("isMuted", false)
+        isSoundOff = sharedPreferences.getBoolean("isSoundOff", false)
+
+        mediaPlayer = MediaPlayerManager.getMediaPlayer(this)
+        btnDefaultMusic = MediaPlayer.create(this, R.raw.button)
+        mediaPlayer.start()
+
+        if(isMuted){
+            mediaPlayer.setVolume(0f,0f)
+        }else{
+            mediaPlayer.setVolume(0.7f, 0.7f)
+        }
+
+        binding.SettingsBtn.setOnClickListener {
+            if(!isSoundOff){
+                btnDefaultMusic.start()
+            }
+            showCustomDialog()
+        }
+
         binding.startGameBtn.setOnClickListener {
+            if(!isSoundOff){
+                btnDefaultMusic.start()
+            }
             val playerXName = binding.playerXName.text.toString()
             val playerOName = binding.playerOName.text.toString()
 
@@ -25,5 +61,82 @@ class EnterNamesActivity : AppCompatActivity() {
             intent.putExtra("playerOName", playerOName)
             startActivity(intent)
         }
+    }
+
+    private fun updateMuteSwitch(sound_switch : Switch) {
+        if(isMuted){
+            sound_switch.isChecked = false
+        }else{
+            sound_switch.isChecked = true
+        }
+    }
+    private fun toggleMuteState() {
+        if (isMuted) {
+            mediaPlayer.setVolume(0.7f, 0.7f)
+        } else {
+            mediaPlayer.setVolume(0f, 0f)
+        }
+        isMuted = !isMuted
+        saveIsMutedToPrefs(isMuted)
+    }
+
+    private fun updateSoundOffSwitch(sound_switch : Switch) {
+        if(isSoundOff){
+            sound_switch.isChecked = false
+        }else{
+            sound_switch.isChecked = true
+        }
+    }
+
+    private fun showCustomDialog() {
+        val dialog = Dialog(this, R.style.CustomDialogTheme)
+        dialog.setContentView(R.layout.settings_popup_activity)
+
+        val close_btn : ImageView = dialog.findViewById(R.id.close_btn)
+
+        close_btn.setOnClickListener{
+            if(!isSoundOff){
+                btnDefaultMusic.start()
+            }
+            dialog.hide()
+        }
+
+        val sound_switch : Switch = dialog.findViewById(R.id.sound_switch)
+
+        updateMuteSwitch(sound_switch)
+
+        sound_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                isMuted = true
+            }else{
+                isMuted = false
+            }
+            toggleMuteState()
+            updateMuteSwitch(sound_switch)
+        }
+
+        val music_switch : Switch = dialog.findViewById(R.id.music_switch)
+        updateSoundOffSwitch(music_switch)
+
+        music_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                isSoundOff = false
+            }
+            else{
+                isSoundOff = true
+            }
+            saveIsSoundOffToPrefs(isSoundOff)
+            updateSoundOffSwitch(music_switch)
+        }
+
+        dialog.show()
+    }
+
+    private fun saveIsSoundOffToPrefs(isSOff: Boolean) {
+        sharedPreferences.edit().putBoolean("isSoundOff", isSOff).apply()
+    }
+
+    private fun saveIsMutedToPrefs(isMute: Boolean) {
+        sharedPreferences.edit().putBoolean("isMuted", isMute).apply()
     }
 }
--- app/src/main/java/com/example/tic_tac_toe_002/GameActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe_002/GameActivity.kt
@@ -3,23 +3,52 @@ package com.example.tic_tac_toe_002
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.SharedPreferences
+import android.media.MediaPlayer
 import android.view.View
+import android.widget.ImageView
+import android.widget.Switch
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import com.example.tic_tac_toe_002.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityGameBinding
+    private lateinit var sharedPreferences: SharedPreferences
     private var playerXName: String = ""
     private var playerOName: String = ""
-
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var btnDefaultMusic: MediaPlayer
     private var gameModel: GameModel? = null
     private var lastClickedPosition: Int = -1
+    private var isMuted: Boolean = false
+    private var isSoundOff : Boolean = false
+    private var settingPopup : Dialog? = null
+    private var winningPopup : Dialog? = null
+    private lateinit var winingMusic: MediaPlayer
+    private lateinit var DrawMusic: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sharedPreferences = getSharedPreferences("HangmanPrefs", Context.MODE_PRIVATE)
+
+        isMuted = sharedPreferences.getBoolean("isMuted", false)
+        isSoundOff = sharedPreferences.getBoolean("isSoundOff", false)
+
+        val mediaPlayerManager = MediaPlayerManager
+        mediaPlayer = mediaPlayerManager.getMediaPlayer(this)
+
+        btnDefaultMusic = MediaPlayer.create(this, R.raw.button)
+        winingMusic = MediaPlayer.create(this, R.raw.game_won)
+        DrawMusic = MediaPlayer.create(this, R.raw.game_lost)
 
         playerXName = intent.getStringExtra("playerXName") ?: "Player X"
         playerOName = intent.getStringExtra("playerOName") ?: "Player O"
@@ -41,12 +70,25 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         binding.XBtn.text = "X"
 
         binding.startGameBtn.setOnClickListener {
+            if(!isSoundOff) {
+                btnDefaultMusic.start()
+            }
             startGame()
             binding.startGameBtn.visibility = View.INVISIBLE
         }
 
         binding.undoMoveBtn.setOnClickListener {
+            if(!isSoundOff) {
+                btnDefaultMusic.start()
+            }
             undoLastMove()
+        }
+
+        binding.SettingsBtn.setOnClickListener {
+            if(!isSoundOff){
+                btnDefaultMusic.start()
+            }
+            showCustomDialog()
         }
 
         GameData.gameModel.observe(this) {
@@ -146,11 +188,15 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
                     gameStatus = GameStatus.FINISHED
                     winner = filledPos[i[0]]
 
+                    var name = ""
+
                     // Increment points for the winner
                     if (winner == "X") {
                         pointsX++
+                        name = playerXName
                     } else {
                         pointsO++
+                        name = playerOName
                     }
 
                     val winnerMessage = if (winner == "X") {
@@ -158,9 +204,11 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
                     } else {
                         "Player O wins!"
                     }
-
+                    if(!isSoundOff){
+                        winingMusic.start()
+                    }
                     // Show popup message
-                    showWinnerPopup(winnerMessage, winner)
+                    showWinnerPopup(winnerMessage, name, "won")
 
                     updateGameData(this)
                     return
@@ -168,8 +216,12 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             if (filledPos.none { it.isEmpty() }) {
+                if(!isSoundOff){
+                    DrawMusic.start()
+                }
                 gameStatus = GameStatus.FINISHED
-                showWinnerPopup("It's a draw!", "")
+                showWinnerPopup("It's a draw!", "", "draw")
+
             }
 
             updateGameData(this)
@@ -194,20 +246,47 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun showWinnerPopup(message: String, winner: String) {
-        binding.startGameBtn.visibility = View.VISIBLE
-        val winnerName = if (winner.isNotEmpty()) {
-            if (winner == "X") playerXName else playerOName
-        } else {
-            "Draw"
-        }
-        AlertDialog.Builder(this)
-            .setTitle("Game Over")
-            .setMessage("$message\nWinner: $winnerName")
-            .setPositiveButton("OK") { dialog, _ ->
-                dialog.dismiss()
+    private fun showWinnerPopup(message: String, winner: String, status : String) {
+
+        val dialog = Dialog(this, R.style.CustomDialogTheme)
+        winningPopup = dialog
+        dialog.setContentView(R.layout.result_popup_activity)
+
+        val close_btn : ImageView = dialog.findViewById(R.id.close_btn)
+
+        close_btn.setOnClickListener{
+            if(!isSoundOff){
+                btnDefaultMusic.start()
             }
-            .show()
+            dialog.hide()
+        }
+
+        val title_txt : TextView = dialog.findViewById(R.id.title_txt_popup)
+        val status_txt : TextView = dialog.findViewById(R.id.status_txt_popup)
+        val status_para : TextView = dialog.findViewById(R.id.status_para_popup)
+
+
+
+        if (status.equals("won")){
+            title_txt.text = "WON !"
+            status_txt.text = winner + " Won"
+            status_para.text = "Congrats on being the undisputed champion of pressing buttons like a pro."
+        }else if (status.equals("draw")){
+            title_txt.text = "Draw !"
+            status_txt.text = "It’s a Draw!"
+            status_para.text = "Congrats to both of you for equally excelling in the art of not winning."
+        }
+
+        dialog.show()
+        binding.startGameBtn.visibility = View.VISIBLE
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        settingPopup?.dismiss()
+        winningPopup?.dismiss()
+
     }
 
     private fun undoLastMove() {
@@ -231,5 +310,83 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
             setUI()
             updateGameData(this)
         }
+    }
+
+    private fun updateMuteSwitch(sound_switch : Switch) {
+        if(isMuted){
+            sound_switch.isChecked = false
+        }else{
+            sound_switch.isChecked = true
+        }
+    }
+    private fun toggleMuteState() {
+        if (isMuted) {
+            mediaPlayer.setVolume(0.7f, 0.7f)
+        } else {
+            mediaPlayer.setVolume(0f, 0f)
+        }
+        isMuted = !isMuted
+        saveIsMutedToPrefs(isMuted)
+    }
+
+    private fun updateSoundOffSwitch(sound_switch : Switch) {
+        if(isSoundOff){
+            sound_switch.isChecked = false
+        }else{
+            sound_switch.isChecked = true
+        }
+    }
+
+    private fun showCustomDialog() {
+        val dialog = Dialog(this, R.style.CustomDialogTheme)
+        settingPopup = dialog
+        dialog.setContentView(R.layout.settings_popup_activity)
+
+        val close_btn : ImageView = dialog.findViewById(R.id.close_btn)
+
+        close_btn.setOnClickListener{
+            if(!isSoundOff){
+                btnDefaultMusic.start()
+            }
+            dialog.hide()
+        }
+
+        val sound_switch : Switch = dialog.findViewById(R.id.sound_switch)
+
+        updateMuteSwitch(sound_switch)
+
+        sound_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                isMuted = true
+            }else{
+                isMuted = false
+            }
+            toggleMuteState()
+            updateMuteSwitch(sound_switch)
+        }
+
+        val music_switch : Switch = dialog.findViewById(R.id.music_switch)
+        updateSoundOffSwitch(music_switch)
+
+        music_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                isSoundOff = false
+            }
+            else{
+                isSoundOff = true
+            }
+            saveIsSoundOffToPrefs(isSoundOff)
+            updateSoundOffSwitch(music_switch)
+        }
+
+        dialog.show()
+    }
+
+    private fun saveIsSoundOffToPrefs(isSOff: Boolean) {
+        sharedPreferences.edit().putBoolean("isSoundOff", isSOff).apply()
+    }
+
+    private fun saveIsMutedToPrefs(isMute: Boolean) {
+        sharedPreferences.edit().putBoolean("isMuted", isMute).apply()
     }
 }
